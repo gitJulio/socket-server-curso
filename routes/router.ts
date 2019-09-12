cd7723ea54d9b36ad934c6a9c045a6e082a7f366
Julio Bueso <julio.ezequiel@gmail.com>
2019-09-12 08:21:36 -0600

    Router socket

--- routes/router.ts
+++ routes/router.ts
@@ -1,16 +1,49 @@
  import { Router, Request, Response } from 'express';
+import Server from '../clases/server';
+
 
 
  export const router =Router();
 
 
+
+ router.post('/mensajes', (req:Request, res: Response)=>{
+    
+    const cuerpo=req.body.cuerpo;
+    const de = req.body.de;
+
+    const payload={cuerpo,de}
+
+    const server = Server.instance;
+    server.io.emit('mensaje-nuevo',payload)
+    res.json({
+        or:true,
+       cuerpo,
+        de
+    })
+
+ })
+
  router.get('/mensajes/:id', (req:Request, res:Response)=>{
+
+    const cuerpo=req.body.cuerpo;
+    const de = req.body.de;
+    const id = req.params.id;
+
+    const payload={
+
+        de,
+        cuerpo
+    }
+
+    const server = Server.instance;
+    server.io.in(id).emit('mensaje-privado',payload);
 
     res.json({
         or:true,
-        mensaje:req.body.cuerpo,
-        de:req.body.de,
-        deurl:req.params.id
+       cuerpo,
+        de,
+        id
     })
 
  })
